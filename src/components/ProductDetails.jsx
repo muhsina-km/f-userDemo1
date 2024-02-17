948f0fd81742bafe618a40e75f18531dc97a0957
muhsina-km <muhsim421@gmail.com>
2024-02-17 06:02:27 -0800

    aaha

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import baseurl from '../Api';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import baseurl from "../Api";
+import { Badge, Col, Image, Row, Tag, Typography } from "antd";
+import colorNames from "colornames";
 
 const ProductDetails = () => {
-
   const { plantid } = useParams();
-
-  const [Plantdetailsview, setPlantdetailsview] = useState([]);
+  const { Text, Title, Paragraph } = Typography;
+  const [Plantdetailsview, setPlantdetailsview] = useState();
 
   useEffect(() => {
     // Fetch the details of the product with id
-    axios.get(`${baseurl}/plantdetails/pview/${plantid}`)
-    .then((response) => {
+    axios
+      .get(`${baseurl}/plantdetails/pview/${plantid}`)
+      .then((response) => {
         console.log(response.data);
-        setPlantdetailsview(response.data);
+        setPlantdetailsview(response.data[0]);
       })
       .catch((err) => console.log(err));
-  }, [plantid])
+  }, [plantid]);
 
   if (!Plantdetailsview) {
     return <div>Loading...</div>; // You can customize the loading state
@@ -25,10 +27,42 @@ const ProductDetails = () => {
 
   return (
     <div>
-      <h2>{Plantdetailsview.plantname} Details</h2>
-      <img src={Plantdetailsview.plantphoto} alt="image" style={{ height: '150px', objectFit: 'cover' }} />
-      <p>Price: ₹{Plantdetailsview.price}</p>
-      <p>Description: {Plantdetailsview.description}</p>
+      <Row gutter={16} style={{ margin: "50px" }}>
+        <Col span={8}>
+          <Image
+            src={Plantdetailsview.plantphoto}
+            alt="image"
+            style={{
+              width: "100%",
+              height: "100%",
+              maxHeight: "400px",
+              objectFit: "cover",
+            }}
+          />
+        </Col>
+        <Col span={12} style={{marginBottom:'7px'}}>
+          <h2>{Plantdetailsview.plantname}</h2>
+          <Title level={4} type="secondary" strong>
+            Price: ₹{Plantdetailsview.price}
+          </Title>
+          <Text type="primary" strong>Size:</Text><Text> {Plantdetailsview.size}</Text>
+          <br />
+          <br />
+          <Text type="secondary">
+            Color:{" "}</Text>
+            <Tag
+            style={{color:'black'}}
+              size="large"
+              color={colorNames(Plantdetailsview.color)
+              }
+            >
+              {Plantdetailsview.color}
+            </Tag>
+          <br />
+          <br />
+          <Paragraph>Description: {Plantdetailsview.description}</Paragraph>
+        </Col>
+      </Row>
       {/* You can display other details of the product */}
     </div>
   );
