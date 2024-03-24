f71c1670424ee2e0302dead624ff70107e92f17b
muhsina-km <muhsim421@gmail.com>
2024-03-24 04:03:10 -0700

    yeye

--- src/components/OrderView.jsx
+++ src/components/OrderView.jsx
@@ -7,12 +7,14 @@ import Footer from './Footer';
 import { Breadcrumb, Card, Button, notification, message, Popconfirm, Space, Image } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
+import { EyeOutlined } from '@ant-design/icons';
 
 const OrderView = () => {
   const [orders, setOrders] = useState([]);
   const [messageApi, contextHolder] = message.useMessage();
   const navigate = useNavigate();
   const userId = localStorage.getItem('user');
+  const email = userId ? JSON.parse(userId).email : null;
 
   const getCurrentDate = (date) => {
     const options = {
@@ -24,17 +26,23 @@ const OrderView = () => {
   }
 
   useEffect(() => {
-    if (userId) {
-      axios.get(`${baseurl}/order/fetch-orders`)
-        .then((response) => {
+    const fetchOrders = async () => {
+      try {
+        if (email) {
+          const response = await axios.get(`${baseurl}/order/fetch-orders`, {
+            params: {
+              email: email
+            }
+          });
           console.log('Fetched Orders:', response.data.orders);
           setOrders(response.data.orders);
-        })
-        .catch((error) => {
-          console.error('Error fetching orders:', error);
-        });
-    }
-  }, [userId]);
+        }
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    };
+    fetchOrders();
+  }, [email]);
 
   const handleCancelOrder = async (orderId) => {
     try {
@@ -65,6 +73,13 @@ const OrderView = () => {
     navigate('/home');
   };
 
+  const handleLinkClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div style={{ backgroundColor: '#FFF5F5', paddingTop: '4px' }}>
       <Navbar />
@@ -89,42 +104,46 @@ const OrderView = () => {
             <Card
               key={order._id}
               hoverable
-              style={{ width: '80%', margin: '16px', marginLeft: '90px', marginRight: '50px'}}
+              style={{ width: '80%', margin: '16px', marginLeft: '90px', marginRight: '50px' }}
             >
               <h3 style={{ textAlign: 'center', marginBottom: '0px', marginTop: '0px', marginLeft: '-90px' }}>
                 Ordered on {getCurrentDate(new Date())}</h3>
               {order.items.map((item, index) => (
                 <div key={index}
                   hoverable
-                  style={{ display: 'flex', alignItems: 'center', marginLeft: '20px'}}>
-                  <Image src={item.plantphoto} alt="plant" 
-                         style={{ height: '100px', width: '120px', objectFit: 'cover' }} />
+                  style={{ display: 'flex', alignItems: 'center', marginLeft: '20px' }}>
+                  <Image src={item.plantphoto} alt="plant"
+                    style={{ height: '100px', width: '120px', objectFit: 'cover' }} />
                   <div style={{ marginLeft: '40px' }}>
                     <p>Product Name: {item.plantname}</p>
                     <p>Price: ₹{item.price}</p>
                     <p>Quantity: {item.quantity}</p>
+                    <Link to={`/view/${item.productId}`} onClick={handleLinkClick}>
+                      <Button type="primary" size='small' icon={<EyeOutlined />}>
+                        View Product
+                      </Button></Link>
                   </div>
                 </div>
               ))}
 
-              <div style={{ marginLeft:'680px', marginTop:'-60px'}}>
-              {order.status !== 'DELIVERED' && (
-                <div style={{ marginLeft:'20px'}}>
-                  <p style={{color: '#007FFF',marginLeft:'18px'}}> <b>ORDERING</b> </p> 
-                  <Popconfirm
-                    title="Are you sure to cancel this order?"
-                    onConfirm={() => confirmRemove(order._id)}
-                    onCancel={cancelRemove}
-                    okText="Yes"
-                    cancelText="No">
-                    <Button type="primary" danger style={{ marginTop: '-60px' }}>
-                      Cancel Order
-                    </Button>
-                  </Popconfirm>
-                </div>
-              )}
+              <div style={{ marginLeft: '680px', marginTop: '-60px' }}>
+                {order.status !== 'DELIVERED' && (
+                  <div style={{ marginLeft: '20px' }}>
+                    <p style={{ color: '#346fe9', marginLeft: '18px' }}> <b>ORDERING</b> </p>
+                    <Popconfirm
+                      title="Are you sure to cancel this order?"
+                      onConfirm={() => confirmRemove(order._id)}
+                      onCancel={cancelRemove}
+                      okText="Yes"
+                      cancelText="No">
+                      <Button type="primary" danger style={{ marginTop: '-60px' }}>
+                        Cancel Order
+                      </Button>
+                    </Popconfirm>
+                  </div>
+                )}
 
-              {order.status === 'DELIVERED' && <p style={{ color: 'green', marginTop: '8px' }}> <b>ORDER DELIVERED</b> <DoneAllIcon style={{marginBottom:'-6px'}}/> </p>}
+                {order.status === 'DELIVERED' && <p style={{ color: 'green', marginTop: '8px' }}> <b>ORDER DELIVERED</b> <DoneAllIcon style={{ marginBottom: '-6px' }} /> </p>}
               </div>
 
             </Card>
